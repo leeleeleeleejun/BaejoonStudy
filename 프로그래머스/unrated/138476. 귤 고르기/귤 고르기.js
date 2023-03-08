27b9d03f6448a3ff767c37f63866f2961c18433f
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-03-08 22:03:58 +0900

    [unrated] Title: 귤 고르기, Time: 63.35 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,30 +1,18 @@
 function solution(k, tangerine) {
     var answer = 0;
-    let target = new Map();
-    for(item of tangerine){
-        if(target.has(item)){
-            target.set(item,target.get(item)+1)
-        }else target.set(item,1)
+    var obj = {};
+    for (item of tangerine){
+        obj[item] = obj[item] > 0 ? obj[item] + 1 : 1;
     }
-    
-    let targetSort = [...target].sort(function(a, b) {
-        return b[1] - a[1];
+    const ary = Object.values(obj).sort(function(a, b) {
+        return b - a;
     })
-    for (item of targetSort){
-        if(k >= item[1]){
-            answer ++;
-            k -= item[1]; 
-        } else if(k !==0 ){
-            answer++
-            break
-        } else {
-            break
-        }
-        
-      
-    }
     
-
     
+  for (item of ary) {
+    answer++;
+    if (k > item) k -= item;
+    else break;
+  }
     return answer;
 }
