34e25ca5f7e5bc4f06b2e6bc0e3fd3aea865ff8a
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-03-09 22:27:31 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.57 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,20 +1,19 @@
 function solution(number, limit, power) {
-  let answer = 0;
-  let array = [];
-  for (let n = 1; n <= number; n++) {
-    let count = 0;
-    for (let i = 1; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) {
+  var answer = 0;
+  var array = [];
+  for (let i = 1; i <= number; i++) {
+      var count = 0;
+    for (let n = 1; n <= Math.sqrt(i); n++) {
+      if (i % n === 0) {
+          count++
+        if (n !== i / n) {
           count++;
-          if (i !== n / i) {
-            count++;
         }
       }
+    }
+    array.push(count)  
   }
-      array.push(count);
-}
-console.log(array)
-  for(item of array){
+    for(item of array){
      answer += item <= limit ? item : power; 
   }
   return answer;
