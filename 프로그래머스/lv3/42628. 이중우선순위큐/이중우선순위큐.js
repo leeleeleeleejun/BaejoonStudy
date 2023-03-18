6da71319085c9bd7cc94c3882e6f4575ea04a32e
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-03-18 12:32:30 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,17 +1,14 @@
 function solution(operations) {
   var answer = [];
   for (const itme of operations) {
-      
     if (itme[0] === 'I') {
       answer.push(Number(itme.replace(/[^0-9-]/g, '')));
     } else {
       answer = answer.sort((a,b) => b-a);
-      if (itme[2] === '1') {
-        answer.shift();
-      } else answer.pop();
+      if (itme[2] === '1') answer.shift();
+      else answer.pop();
     }
       answer = answer.sort((a,b) => b-a);
   }
-
   return answer.length === 0 ? [0,0] : [answer[0], answer[answer.length-1]];
 }
