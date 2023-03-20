2e0992358bdb468c5475add6cb8a64185c8765c0
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-03-21 02:26:08 +0900

    [level 1] Title: 소수 찾기, Time: 64.05 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -2,7 +2,7 @@ function solution(n) {
     var answer = 1;
     let array = [2];
     for(let j = 2; j <= n; j++){
-        if(j > 3 && (j%2 === 0 || j%3 === 0))continue;
+        //if(j > 3 && (j%2 === 0 || j%3 === 0))continue;
         for(let i = 0; i < array.length; i++){
             if (j%array[i] === 0) {
                 break;
