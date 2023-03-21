3e4dc687d3a4b9a28283c105a8c76980eee31401
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-03-22 00:09:28 +0900

    [level 2] Title: 구명보트, Time: 11.57 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,14 +1,12 @@
 function solution(people, limit) {
   var answer = 0;
-  const aa = people.filter((item)=>(limit - item) >= 40);
-  answer += people.length - aa.length;
+  const aa = people
+  //answer += people.length - aa.length;
   aa.sort((a, b) => b - a)
   for(let i = 0; i < aa.length; i++){
     if(aa[i]+aa[aa.length-1] > limit){
-       // console.log('a',aa[i])
       answer++;
     }else{
-        //console.log('b',aa[i])
       answer++;
       aa.pop();
     }
