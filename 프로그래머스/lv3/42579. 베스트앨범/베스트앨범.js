aa39cc5aa4d8fc97c1d2869ac771818f52ed7476
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-03-23 00:52:20 +0900

    [level 3] Title: 베스트앨범, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,26 @@
+function solution(genres, plays) {
+  var answer = [];
+  const map = new Map();
+  
+  for (let i = 0; i < genres.length; i++) {
+    if (!map.has(genres[i])) {
+      map.set(genres[i], {totalPlays: plays[i], songs: [{index: i, plays: plays[i]}]});
+    } else {
+      const genreInfo = map.get(genres[i]);
+      genreInfo.totalPlays += plays[i];
+      genreInfo.songs.push({index: i, plays: plays[i]});
+    }
+  }
+
+  const sortedGenres = [...map].sort((a, b) => b[1].totalPlays - a[1].totalPlays);
+
+  for (const genreInfo of sortedGenres) {
+    const sortedSongs = genreInfo[1].songs.sort((a, b) => b.plays - a.plays);
+    answer.push(sortedSongs[0].index);
+    if (sortedSongs.length > 1) {
+      answer.push(sortedSongs[1].index);
+    }
+  }
+
+  return answer;
+}
