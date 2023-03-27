27556ca1354ecae8cbab9d8f17f0d21f60b1c191
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-03-28 01:01:10 +0900

    [level 2] Title: 소수 찾기, Time: 0.55 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -2,8 +2,10 @@ function solution(numbers) {
   const set = new Set();
   var answer = 0;
   numbers = numbers.split("");
-
+  
+    // 모든 조합을 구하는 재귀함수
   function recursive(comb, others) {
+      // comb를 기준으로 조합 추가
     if (comb.length !== 0) set.add(Number(comb.join("")));
     for (let i = 0; i < others.length; i++) {
       recursive(
@@ -16,8 +18,10 @@ function solution(numbers) {
   recursive([], numbers);
 
   for(const item of [...set]){
+      // 모든 조합을 순회하며 소수 확인
     if(isPrime(item)) answer++
   }
+    // 소수를 구하는 함수
   function isPrime(num) {
     if(num === 0 || num === 1) return false;
     for (let i = 2; i <= Math.sqrt(num); i++) {  
