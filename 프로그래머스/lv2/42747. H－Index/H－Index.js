d8488a9c2f770735c6b4ca8eeef91eb31a340226
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-03-28 00:43:07 +0900

    [level 2] Title: H-Index, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,9 +1,8 @@
 function solution(citations) {
-    citations.sort((a, b) => b - a); // 내림차순 정렬
-    for (let i = 0; i < citations.length; i++) {
-        if (citations[i] <= i) {
-            return i; // H-index 반환
-        }
+    var answer = 0;
+    citations.sort((a,b)=>b-a)
+    for(let i = 0; i <= citations.length; i++){
+        if(i <= citations.filter(item => i <= item).length) answer = i;
     }
-    return citations.length; // 모든 논문이 인용된 경우
+    return answer;
 }
