b813d685008deb947ccb31d403c3cae70dd1c836
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-03-30 20:58:05 +0900

    [level 2] Title: 소수 찾기, Time: 0.59 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,11 +1,13 @@
 function solution(numbers) {
+    // 숫자의 조합이 들어갈 객체
+    // 
   const set = new Set();
   var answer = 0;
   numbers = numbers.split("");
-  
-    // 모든 조합을 구하는 재귀함수
+
+  // 모든 조합을 구하는 재귀함수
   function recursive(comb, others) {
-      // comb 추가
+    // comb 추가
     if (comb.length !== 0) set.add(Number(comb.join("")));
     for (let i = 0; i < others.length; i++) {
       recursive(
@@ -17,17 +19,26 @@ function solution(numbers) {
 
   recursive([], numbers);
 
-  for(const item of [...set]){
-      // 모든 조합을 순회하며 소수 확인
-    if(isPrime(item)) answer++
+  // 재귀함수가 불러지는 과정
+  // 1. recursive([], [1, 2, 3])
+  // 2. recursive([1], [2, 3]) , ///// recursive([2], [1, 3]) , recursive([3], [2, 3])
+  // 3. recursive([1, 2], [3]) , recursive([1, 3], [2])
+  // 4. recursive([1, 2, 3], []) , recursive([1, 3, 2], [])
+
+  for (const item of [...set]) {
+    // 모든 조합을 순회하며 소수 확인
+    if (isPrime(item)) answer++;
   }
-    // 소수를 구하는 함수
+    
+    
+  // 소수를 구하는 함수
   function isPrime(num) {
-    if(num === 0 || num === 1) return false;
-    for (let i = 2; i <= Math.sqrt(num); i++) {  
+    if (num === 0 || num === 1) return false;
+      // 에라토스테네스의 채
+    for (let i = 2; i <= Math.sqrt(num); i++) {
       if (num % i === 0) return false;
     }
-      return true;
+    return true;
   }
 
   return answer;
