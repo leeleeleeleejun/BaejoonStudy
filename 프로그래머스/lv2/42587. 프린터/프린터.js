305154a87a704f718e49ffa09200fdee2d2122c1
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-04-05 00:53:08 +0900

    [level 2] Title: 프린터, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.js
@@ -0,0 +1,24 @@
+function solution(priorities, location) {
+    var answer = 0;
+    let map = new Map();
+    for(let i = 0; i < priorities.length; i++){
+        map.set(i,priorities[i])
+    }
+    map = [...map]
+    priorities.sort((a,b)=> b-a)
+    while(true){
+        if(map[0][1] === priorities[0]){
+            if(map[0][0] === location){
+                break;
+            }
+            answer++
+            map.shift();
+            priorities.shift();
+        }else{
+            map.push(map[0]);
+            map.shift();
+        }
+        
+    }
+    return answer+1;
+}
