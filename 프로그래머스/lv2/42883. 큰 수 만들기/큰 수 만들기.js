7b95927fb68cbaff53ffa3f127844753a04701b2
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-05-27 15:17:34 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,23 +1,21 @@
 function solution(number, k) {
-  const stack = [];
-  let count = 0;
-  
-  for (let i = 0; i < number.length; i++) {
-    const current = number[i];
+    var answer = '';
+    const stack = [];
+    let count = 0;
     
-    while (stack.length > 0 && stack[stack.length - 1] < current && count < k) {
-      stack.pop();
-      count++;
+    for (let i = 0;  i < number.length; i ++){
+        const currNum = number[i]
+        while(stack.length > 0 && stack[stack.length-1] < currNum && count < k){
+            stack.pop();
+            ++count;
+        }
+        stack.push(currNum)
     }
     
-    stack.push(current);
-  }
-  
-  // k개의 숫자를 제거하지 못한 경우, 남은 숫자 중에서 뒤에서부터 제거합니다.
+      // k개의 숫자를 제거하지 못한 경우, 남은 숫자 중에서 뒤에서부터 숫자를 제거합니다.
   while (count < k) {
     stack.pop();
     count++;
   }
-  
-  return stack.join('');
+    return stack.join('');
 }
