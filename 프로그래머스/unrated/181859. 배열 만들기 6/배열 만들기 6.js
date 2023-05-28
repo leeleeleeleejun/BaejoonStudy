2ec2236589a55f8eccde3c7b29f39ca298d80cde
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-05-28 14:20:28 +0900

    [unrated] Title: 배열 만들기 6, Time: 3.46 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    var answer = [];
+    const stk = []
+    for(let i = 0; i < arr.length; i++){
+        if(stk.length > 0){
+            if(stk[stk.length-1] === arr[i]){
+                stk.pop();
+            }else {
+                stk.push(arr[i]);
+            }
+        }else{
+            stk.push(arr[i]);
+        }
+    }
+    return stk.length > 0 ? stk : [-1];
+}
