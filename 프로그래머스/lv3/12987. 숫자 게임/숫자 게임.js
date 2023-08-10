34796d636453cc9214c7725ee1af37fb2cc75c81
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2023-08-10 13:21:08 +0900

    [level 3] Title: 숫자 게임, Time: 79.24 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
@@ -2,11 +2,11 @@ function solution(A, B) {
     var answer = 0;
     A.sort((a,b) => b-a);
     B.sort((a,b) => b-a);
-    let aa = 0
+    let B_Index = 0
     for(let i = 0; i < A.length; i++){
-        if(A[i] < B[aa]){
+        if(A[i] < B[B_Index]){
             answer++;
-            aa++;
+            B_Index++;
         }
     }
     return answer;
