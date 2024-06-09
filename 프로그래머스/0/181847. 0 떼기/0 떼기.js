e1cf5ff61d51163e7b649199c497dbe278ee364d
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2024-06-10 00:10:54 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -2,7 +2,6 @@ function solution(n_str) {
     var answer = '';
     for(let i = 0; i < n_str.length; i++){
         if(n_str[i] !== '0'){
-            console.log(i)
             answer = n_str.slice(i);
             break;
         }
