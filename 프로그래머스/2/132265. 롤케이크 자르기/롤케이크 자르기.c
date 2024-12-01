896262ba21c3521729df62f964e341fd761010e7
leeleeleeleejun <98197264+leeleeleeleejun@users.noreply.github.com>
2024-12-01 22:37:53 +0900

    [level 2] Title: 롤케이크 자르기, Time: 3.90 ms, Memory: 32.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// topping_len은 배열 topping의 길이입니다.
+int solution(int topping[], size_t topping_len) {
+    int answer = -1;
+    return answer;
+}
